8d405b1a1563f99512bcde8d7e4d1b01329b45cb
Subham Bhandari <subham.bhandari@globallogic.com>
2023-04-11 15:25:45 +0530

    Added Code Splitting

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,16 +1,18 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import {Routes, Route} from "react-router-dom";
 import Stateupdate from "./component/Stateupdate";
-import MyForm from "./component/Form";
-import Contact from "./component/Contact";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
-// const Contact = lazy(() => import("./component/Contact"));
+
+//Lazy loaded Code Splitting
+const MyForm = lazy(() => import("./component/Form"));
+const Contact = lazy(() => import("./component/Contact"));
 
 function App() {
   return (
     <>
-    <Navbar />
+  <Suspense fallback ={<h1>Loading....</h1>}>
+       <Navbar />
 
     <Routes >
       <Route path="/" element={ <Home /> } />
@@ -18,6 +20,7 @@ function App() {
       <Route path="/stateUpdate" element={ <Stateupdate /> } />
       <Route path="/contact" element={ <Contact /> } />
     </Routes>
+  </Suspense>
     </>
   );
 }
